bcffbfb94ce174ae98972f75fe1a15f69e916412
Khalifa Bako <khalifabako@hotmail.com>
2023-02-16 04:21:27 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A Program that prints the size of various types on the sum
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
