1f52fc28719f601ccb0bc5132f647bc069ac3e81
Khalifa Bako <khalifabako@hotmail.com>
2023-02-16 04:29:12 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,3 +11,4 @@ printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
+}
