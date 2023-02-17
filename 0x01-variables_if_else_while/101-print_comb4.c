8b17e9fa2233ea591e35154ebed61c5a4f68088b
Khalifa Bako <khalifabako@hotmail.com>
2023-02-17 08:50:48 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.b>
+/**
+ * main - Prints all possible different combinations of three digits.
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+int c;
+int d;
+int e = 0;
+while (e < 10)
+{
+d = 0;
+while (d < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (c != d && d != e && e < d && d < c)
+{
+putchar('0' + e);
+putchar('0' + d);
+putchar('0' + c);
+if (c + d + e != 9 + 8 + 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+e++;
+}
+putchar('\n');
+return (0);
+}
