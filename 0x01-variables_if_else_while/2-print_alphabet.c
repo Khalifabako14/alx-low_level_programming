665442c2a4624595146016953e3b16005a9d71fa
Khalifa Bako <khalifabako@hotmail.com>
2023-02-17 05:41:05 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+return (0);
+}
