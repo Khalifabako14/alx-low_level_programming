40ab8dfec30ab7c372c722f3efde4aa4acf7138d
Khalifa Bako <khalifabako@hotmail.com>
2023-02-17 08:27:22 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all possible different combinations of two digits
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+int c;
+int d = 0;
+while (d < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('0' + c);
+
+if (c + d != 17)
+{
+putchar(',')
+putchar(' ')
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
