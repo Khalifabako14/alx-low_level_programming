c5a10433dde61ce583181d23922d25f07f293d85
Khalifa Bako <khalifabako@hotmail.com>
2023-02-21 15:47:54 -0800

    0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * Return: 0 (Succesful)
  */
 
 int main(void)
 {
 	int i;
 
-	i = 0
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
