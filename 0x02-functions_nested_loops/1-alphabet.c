e5d63837c21cbc2d9b8e08acacd5ea74a4945a6d
Khalifa Bako <khalifabako@hotmail.com>
2023-02-21 03:33:05 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alpha in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	_putchar('\n');
+}
