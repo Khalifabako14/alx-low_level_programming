25670b290d65d25b3fa44d7d88106e30a6fa8813
Khalifa Bako <khalifabako@hotmail.com>
2023-02-21 07:33:57 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+ /**
+  * print_last_digit - Prints the last digit of a number
+  * @v: the number
+  * Return: value of the last digit
+  */
+
+int print_last_digit(int v)
+{
+	return (v % 10);
+}
