007e484e8f7271a0959a3834cbd58a63382098c3
Khalifa Bako <khalifabako@hotmail.com>
2023-02-27 01:58:37 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "khalifa.h"
+
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s : s is a character
+ * Return: i as a value
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
