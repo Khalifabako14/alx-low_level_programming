6722ac20d30fb5087bb3e13f1a90952bca6ce498
Khalifa Bako <khalifabako@hotmail.com>
2023-02-27 03:28:07 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "khalifa.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, k;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (k = 0; k < len; k++)
+	{
+		dest[k] = src[k];
+	}
+	dest[k] = '\0';
+
+	return (dest);
+}
