6e3a3b2d61b529e380b761fa8d6fccf9aed37f9e
Khalifa Bako <khalifabako@hotmail.com>
2023-02-27 01:34:12 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "khalifa.h"
+
+/**
+ * reset_to_98 - Check the code for Khalifa School students.
+ * @n : n is the pointer of n and return value 98
+ * Return: 0 (Successful)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
