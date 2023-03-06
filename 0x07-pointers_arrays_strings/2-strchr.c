37cabc3aebf6b825b2d53808df5dc24d7ba90d5b
Khalifa Bako <khalifabako@hotmail.com>
2023-03-06 03:13:16 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+nclude "bako.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Function that locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ * Return: A pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int f;
+
+	while (1)
+	{
+		u = *s++;
+		if (u == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
