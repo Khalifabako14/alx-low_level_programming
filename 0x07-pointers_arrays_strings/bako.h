e3bc4ee7b3c3187fe4144de1e5116d53da01ec8a
Khalifa Bako <khalifabako@hotmail.com>
2023-03-06 06:19:20 -0800

    bako.h

--- 0x07-pointers_arrays_strings/bako.h
+++ 0x07-pointers_arrays_strings/bako.h
@@ -1,6 +1,8 @@
 #ifndef BAKO_H
 #define BAKO_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
