f939ef79c2fcb0202b4ed26fc77abdabf8d409f7
Khalifa Bako <khalifabako@hotmail.com>
2023-03-06 03:23:43 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "bako.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Function that searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: stringcontaining the bytes to look for
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a, u;
+
+	for (a = 0; *s != '\0'; a++)
+	{
+		for (u = 0; accept[u] != '\0'; u++)
+		{
+			if (*s == accept[u])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
