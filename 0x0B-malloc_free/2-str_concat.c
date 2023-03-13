c2c9704f028c4ece07b459206101ff9f0bcb9223
Khalifa Bako <khalifabako@hotmail.com>
2023-03-13 03:14:54 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,43 +8,35 @@
  * The function should return NULL on failure
  * @s1: This is the output string
  * @s2: This is the input string
- * Return: The returned pointer should point to a newly allocated space in
- * memory which contains the contents of s1, followed by the contents
- * of s2, and null terminated
+ * Return: If concatenation fails - NULL.
+ * Otherwise - a pointer the newly-allocated space in memory
+ * containing the concatenated strings.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	int *s;
+	char *concat_str;
+	int k, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
 
-	if (s == NULL)
+	for (k = 0; s1[k] || s2[k]; k++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-		s[a] = s1[a];
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-		s[a] = s2[b];
-		a++;
-	}
-	return (s);
+
+	for (k = 0; s1[k]; k++)
+		concat_str[concat_index++] = s1[k];
+
+	for (k = 0; s2[k]; k++)
+		concat_str[concat_index++] = s2[k];
+
+	return (concat_str);
 }
