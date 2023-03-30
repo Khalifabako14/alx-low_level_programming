69873455f0d874e310790be05382434c21536520
Khalifabako14 <khalifabako@hotmail.com>
2023-03-30 10:06:29 -0700

    Singlr Linked Lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - Function that prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t khalifa;
+
+	khalifa = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		khalifa++;
+	}
+	return (khalifa);
+}
