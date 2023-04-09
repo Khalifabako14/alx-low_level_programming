edc229cf9f3c89452ae76a5f953e05226f950d94
Khalifabako14 <khalifabako@hotmail.com>
2023-04-09 06:51:02 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int len = 0;
+	unsigned int total, power;
+	int len;
 
-	if (b[len] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
+	for (len = 0; b[len]; len++)
 	{
-		n <<= 1;
-		n += b[len] - '0';
-		len++;
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
 
-	return (n);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
