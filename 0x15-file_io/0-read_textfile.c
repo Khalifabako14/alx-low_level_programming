1842af7ac5f642eeae62a21b508162360afcfd56
Khalifabako14 <khalifabako@hotmail.com>
2023-04-09 14:19:15 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - Function that reads a text file
  * and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letters: The number of letters the function should read and print.
+ * @letters: The number of letters the
+ * function should read and print.
  * Return: If the function fails or filename is NULL - 0.
  * O/w - the actual number of bytes the function can read and print.
  */
