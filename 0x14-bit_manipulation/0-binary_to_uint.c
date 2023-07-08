89213249ac29618653e08abdec3347ada1d39f4a
Khalifa Bako <khalifabako@hotmail.com>
2023-07-07 17:04:02 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - A fucntion that converts binary string to uint
+ * @b: string with binary num
+ * Return: unsigned int or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num;
+
+	if (!b)
+		return (0);
+
+	for (num = 0; *b; b++)
+	{
+		if (*b == '1')
+			num = (num << 1) | 1;
+		else if (*b == '0')
+			num <<= 1;
+		else
+			return (0);
+	}
+	return (num);
+}
